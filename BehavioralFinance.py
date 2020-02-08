e35803affe9c03d401d683c034b563bf29bb9627
Albert Yeo <54228034+albertyeo@users.noreply.github.com>
2020-02-08 16:10:34 +0800

    Update BehavioralFinance.py

--- BehavioralFinance.py
+++ BehavioralFinance.py
@@ -1,24 +1,31 @@
 #Behavioral Finance
+
+'''Monte Carlo simulation of utility function based on Barberis, Huang, and 
+Santos (2001) economy to calculate the price-dividend ratio for market 
+portfolio and expected market return. The results are plotted against the 
+amount of emphasis that investor puts on utility from financial gain or loss, 
+compared to the utility of consumption.
+'''
 #------------------------------------------------------------------------------
 import numpy as np
 import matplotlib.pyplot as plt
-#------------------------------------------------------------------------------
+
 def BF(theta, gamma, lumbda, array_rf):
     #Consumption growth function
     g = np.array([np.exp(0.02 + 0.02 * np.random.normal()) 
-                for i in range (10000)]) #Check the function of g
+                for i in range (10000)])
     
     #Utility from financial gain or loss
     def nu_R(R):
-        return [i - array_rf if i >= array_rf else lumbda * (i - array_rf) for i in R]
+        return [i - array_rf if i >= array_rf else lumbda * (i - array_rf) 
+                for i in R]
     
     #Error term
     def e_term(x, b_0):
-        return theta * b_0 * np.mean(nu_R(x*g)) + theta * x - 1 #check function of error term
-        #this e_term formula assumes
+        return theta * b_0 * np.mean(nu_R(x*g)) + theta * x - 1
     
     #Bisection search
-    b_0 = np.linspace(0, 10, 101) #check the linspace value (0, 10, 101?)
+    b_0 = np.linspace(0, 10, 101)
     x_values = []
     def x_term(x_neg, x_pos, b_0):
         x = 0.5 * (x_neg + x_pos)
@@ -30,7 +37,7 @@ def BF(theta, gamma, lumbda, array_rf):
             x_term(x_neg, x, b_0)
     
     for i in b_0:
-        x_term(1, 1.1, i) #check the value of xneg,xpos (1,1.1?)
+        x_term(1, 1.1, i)
     
     #Price-Dividend ratio
     n = len(x_values)
